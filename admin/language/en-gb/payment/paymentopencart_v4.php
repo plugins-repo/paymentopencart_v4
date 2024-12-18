4fc4e58625267b7152567db21d44c5ba89108098
plugins-repo <mahesh.biradar@paymentz.com>
2024-12-18 16:13:39 +0530

    Update paymentopencart_v4.php

--- admin/language/en-gb/payment/paymentopencart_v4.php
+++ admin/language/en-gb/payment/paymentopencart_v4.php
@@ -1,1 +1,26 @@
+<?php
+// Heading
+$_['heading_title'] = 'Paymentopencart_v4';
+
+// Text
+$_['text_extension'] = 'Extensions';
+$_['text_success'] = 'Success: You have modified Paymentopencart_v4 payment module!';
+$_['text_edit'] = 'Edit Paymentopencart_v4 Payment';
+
+// Entry
+$_['entry_merchant_id'] = 'Merchant ID';
+$_['entry_partner_name'] = 'Partner Name';
+$_['entry_merchantRedirectUrl'] = 'Redirect Url';
+$_['entry_secretkey'] = 'Secret Key';
+$_['entry_test_url'] = 'Test/Live Url';
+$_['entry_status'] = 'Status';
+
+// Help
+$_['help_merchant_id'] = 'Enter your merchant ID';
+$_['help_test_url'] = 'Enter the test URL for payment';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify Paymentopencart_v4 payment module!';
+$_['error_merchant_id'] = 'Merchant ID required!';
+$_['error_test_url'] = 'Test URL required!';
 

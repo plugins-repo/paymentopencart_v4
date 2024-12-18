ee7c8c6b5fccfb5ab9b96707855b2dcd4769ae86
plugins-repo <mahesh.biradar@paymentz.com>
2024-12-18 16:16:39 +0530

    Update paymentopencart_v4.php

--- catalog/language/en-gb/payment/paymentopencart_v4.php
+++ catalog/language/en-gb/payment/paymentopencart_v4.php
@@ -1,1 +1,7 @@
+<?php
+// Heading
+$_['heading_title'] = 'Paymentopencart_v4';
+$_['text_instruction'] = 'Paymentopencart_v4 Payment Instructions';
+$_['text_payment'] = 'Your order will be processed once payment is confirmed.';
+$_['text_total'] = 'Total Amount:';
 
